396d530bfe2a859b8fbcb407a0b1b8437fc9b564
Nazarov Andrei <nazarov.a.dev@gmail.com>
2020-06-21 23:55:52 +0300

    docker compose and postgres dockerfile

---
+++ projects-service/src/main/java/dev/tadite/habits/projects/ProjectRepository.java
@@ -0,0 +1,6 @@
+package dev.tadite.habits.projects;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ProjectRepository extends PagingAndSortingRepository<Project, Long> {
+}
