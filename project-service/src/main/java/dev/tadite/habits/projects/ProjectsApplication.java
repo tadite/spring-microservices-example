a02fde43ff0f9a08854a735347b5120e76b1eb91
Nazarov Andrei <nazarov.a.dev@gmail.com>
2020-07-08 20:02:03 +0300

    project-service migration to config-server usage

--- project-service/src/main/java/dev/tadite/habits/projects/ProjectsApplication.java
+++ project-service/src/main/java/dev/tadite/habits/projects/ProjectsApplication.java
@@ -2,10 +2,12 @@ package dev.tadite.habits.projects;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class ProjectsApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectsApplication.class, args);
